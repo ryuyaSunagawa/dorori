8fca005f9d6a63160400f747af923933a3175f9b
ryuyaSunagawa <11806134@stu.kbc.ac.jp>
2020-03-12 10:21:50 +0900

    MasterFirstCommit

---
+++ Assets/OwnFile/Nohina/TestScene/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+	//プレイヤーのスピード
+	[Range( 0, 10 )]
+	[SerializeField] float playerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		if( GameManager.Instance.padMode )
+		{
+			//パッド移動
+			float horizontal = Input.GetAxis( "Horizontal" );
+			float vertical = Input.GetAxis( "Vertical" );
+		}
+		else
+		{
+
+		}
+
+    }
+}
---
+++ Assets/spin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+
+	[SerializeField] Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.RotateAround( player.position, transform.up, 20 * Time.deltaTime );
+    }
+}
